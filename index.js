87790d03bb26bd8f6c65a98e46dec6e9b895ad07
Luis Londono <lhl49@cornell.edu>
2019-06-25 20:04:11 -0700

    Revert "header and nav code"
    This reverts commit 93917486182f80d61a01d010b3a69aa6ca3640ce.

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Header from "./components/header"
-
-ReactDOM.render(
-    <Header />,
-    document.getElementById("header-nav")
-  )
