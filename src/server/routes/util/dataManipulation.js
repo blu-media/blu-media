a8f21c25c81b1a296a695b3f5616d57f9c178022
Abenazer <abenazer101@gmail.com>
2019-07-01 14:14:59 -0500

    Set up fake data insertion for the user and organization models using the faker api for adding dummy  data on command

--- src/server/routes/util/dataManipulation.js
+++ src/server/routes/util/dataManipulation.js
@@ -2,22 +2,25 @@ const { db } = require("../../db/connection");
 const faker = require("faker");
 const random = require("random");
 
-const { sequelize } = require('../../db/connection');
+const { sequelize } = require("../../db/connection");
 
 var addUsers = (request, response) => {
   var i;
   for (i = 0; i < request.params.num; i++) {
-    db.users
-      .create({
-        email: "abenazer101@gmail.com",
-        firstName: "Abenazer",
-        gradYear: "2022",
-        lastName: "Mekete",
-        organizations: ["URMC", "BSU", "ESSA", "CRC"],
-        password: "123456789",
-        profilePicture: "profile.jpg",
-        userId: "1"
-      });
+    db.users.create({
+      email: faker.internet.email(),
+      firstName: faker.name.firstName(),
+      gradYear: random.int(2019, 2022),
+      lastName: faker.name.lastName(),
+      organizations: [
+        faker.company.companyName(),
+        faker.company.companyName(),
+        faker.company.companyName()
+      ],
+      password: faker.lorem.word(),
+      //profilePicture: faker.image.image(),
+      userId: faker.lorem.word()
+    });
   }
 
   response.send(`${request.params.num} users created!`);
@@ -26,16 +29,19 @@ var addUsers = (request, response) => {
 var addOrganizations = (request, response) => {
   var i;
   for (i = 0; i < request.params.num; i++) {
-    db.organizations
-      .create({
-        about: faker.lorem.paragraph(),
-        acronym: faker.company.companySuffix(),
-        // contactInfo:
-        // groups: [faker.company.companyName(),faker.company.companyName(),faker.company.companyName()] ,
-        // logo:
-        name: faker.company.companyName(),
-        organizationId: faker.lorem.word()
-      });
+    db.organizations.create({
+      about: faker.lorem.paragraph(),
+      acronym: faker.company.companySuffix(),
+      // contactInfo:
+      groups: [
+        faker.company.companyName(),
+        faker.company.companyName(),
+        faker.company.companyName()
+      ],
+      //logo: faker.image.avatar(),
+      name: faker.company.companyName(),
+      organizationId: faker.lorem.word()
+    });
   }
 
   response.send(`${request.params.num} organizations created!`);
@@ -45,40 +51,42 @@ var addEvents = (request, response) => {
   var i;
 
   for (i = 0; i < request.params.num; i++) {
-    db.events
-      .create({
-        attendees: [
-          faker.name.findName(),
-          faker.name.findName(),
-          faker.name.findName(),
-          faker.name.findName(),
-          faker.name.findName()
-        ],
-        // attire:
-        blurb: faker.lorem.paragraph(),
-        date: faker.date.future(),
-        endTime: faker.date.future(),
-        eventId: faker.lorem.word(),
-        // images:
-        // flyer:
-        location: faker.address.streetName(),
-        name: faker.lorem.word(),
-        // organizations:
-        // rsvps:
-        startTime: faker.date.future(),
-        type: faker.lorem.word()
-      });
+    db.events.create({
+      attendees: [
+        faker.name.findName(),
+        faker.name.findName(),
+        faker.name.findName(),
+        faker.name.findName(),
+        faker.name.findName()
+      ],
+      attire: faker.commerce.color(),
+      blurb: faker.lorem.paragraph(),
+      date: faker.date.future(),
+      endTime: faker.date.future(),
+      eventId: faker.lorem.word(),
+      //images: [faker.image.image(), faker.image.image(), faker.image.image()],
+      //flyer: faker.image.image(),
+      location: faker.address.streetName(),
+      name: faker.lorem.word(),
+      organizations: [
+        faker.company.companyName(),
+        faker.company.companyName(),
+        faker.company.companyName()
+      ],
+      // rsvps:
+      startTime: faker.date.future(),
+      type: faker.lorem.word()
+    });
   }
 
   response.send(`${request.params.num} events created!`);
 };
 
 const wipeDatabase = (request, response) => {
-  sequelize.sync({ force: true })
-    .then(() => {
-      response.send('Database wiped successfully and have been recreated!');
-    });
-}
+  sequelize.sync({ force: true }).then(() => {
+    response.send("Database wiped successfully and have been recreated!");
+  });
+};
 
 module.exports = {
   addUsers,
