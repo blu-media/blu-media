f9e955f264e6110ef859dbaa2776d5d155525fc2
Abenazer <abenazer101@gmail.com>
2019-07-26 22:00:32 -0500

    addev navbar components for mobile webapp version

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles/main.css';
-import NavBar from './components/NavBar.jsx'
+import NavBarOnPhone from './components/NavBar.jsx'
 import Home from './components/Home.jsx'
 import Events from './components/Events.jsx'
 import Organizations from './components/Organizations.jsx'
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        <NavBarOnPhone />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/events" component={Events}/>
-          <Route path="/organizations" component={Organizations}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/events" component={Events} />
+          <Route path="/organizations" component={Organizations} />
         </Switch>
       </div>
     </Router>
--- src/client/src/components/NavBar.jsx
+++ src/client/src/components/NavBar.jsx
@@ -2,14 +2,76 @@ import React from 'react';
 import logo from '../assets/cubal-logo.png';
 import '../styles/NavBar.css';
 import { Link } from 'react-router-dom'; //To set path and route to compent
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 
-class NavBar extends React.Component {
+// class NavBar extends React.Component {
+//     render() {
+//         return (
+//             <header>
+//                 <Navbar bg="light" variant="light">
+//                     <div id="header-logo-wrapper">
+//                         <Navbar.Brand href="#home">
+//                             <a href="/">
+//                                 <img
+//                                     src={logo}
+//                                     width="30"
+//                                     height="30"
+//                                     className="d-inline-block align-top"
+//                                     alt="React Bootstrap logo"
+//                                 />
+//                             </a>
+//                         </Navbar.Brand>
+//                     </div>
+//                     <section id="header-right-wrapper">
+//                         <Nav className="mr-auto">
+//                             <Link to='/' >
+//                                 <Nav.Link href="#home">Home</Nav.Link>
+//                             </Link>
+//                             <Link to='/events'>
+//                                 <Nav.Link href="#events">Events</Nav.Link>
+//                             </Link>
+//                             <Link to='/organizations'>
+//                                 <Nav.Link href="#organizations">Organizations</Nav.Link>
+//                             </Link>
+//                             {/* <div class="buttons-wrapper">
+//                                 <button id="log-in-button">Log in</button>
+//                                 <button id="sign-up-button">Sign Up</button>
+//                             </div> */}
+//                         </Nav>
+//                     </section>
+//                 </Navbar>
+//             </header>
+//         );
+//     }
+// }
+
+class NavBarOnPhone extends React.Component {
     render() {
         return (
             <header>
                 <Navbar bg="light" variant="light">
+                    <div id="header-menu-wrapper">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                MENU
+                        </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Link to='/'>
+                                    <Dropdown.Item href="#/action-1">Home</Dropdown.Item>
+                                </Link>
+                                <Link to='/events'>
+                                    <Dropdown.Item href="#/action-2">Events</Dropdown.Item>
+                                </Link>
+                                <Link to='/organizations'>
+                                    <Dropdown.Item href="#/action-3">Orgnaizations</Dropdown.Item>
+                                </Link>
+                                <Link to='/organizations'>
+                                    <Dropdown.Item href="#/action-3">My CUBAL</Dropdown.Item>
+                                </Link>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
                     <div id="header-logo-wrapper">
                         <Navbar.Brand href="#home">
                             <a href="/">
@@ -23,28 +85,10 @@ class NavBar extends React.Component {
                             </a>
                         </Navbar.Brand>
                     </div>
-                    <section id="header-right-wrapper">
-                        <Nav className="mr-auto">
-                            <Link to='/' >
-                                <Nav.Link href="#home">Home</Nav.Link>
-                            </Link>
-                            <Link to='/events'>
-                                <Nav.Link href="#events">Events</Nav.Link>
-                            </Link>
-                            <Link to='/organizations'>
-                                <Nav.Link href="#organizations">Organizations</Nav.Link>
-                            </Link>
-                            {/* <div class="buttons-wrapper">
-                                <button id="log-in-button">Log in</button>
-                                <button id="sign-up-button">Sign Up</button>
-                            </div> */}
-                        </Nav>
-                    </section>
                 </Navbar>
             </header>
-        );
+        )
     }
 }
 
-
-export default NavBar;
+export default NavBarOnPhone;
